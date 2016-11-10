17bab7b13d2072e651a9f19691d63371be282882
fabio <fabio.resner>
2016-11-10 19:17:20 -0200

    Initial commmit

---
+++ src/main/java/org/fbsks/certservices/CertServicesApplication.java
@@ -0,0 +1,12 @@
+package org.fbsks.certservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertServicesApplication.class, args);
+	}
+}
